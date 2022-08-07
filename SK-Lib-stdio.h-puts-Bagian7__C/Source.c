8ca4844029909b8eb0becb6dda588cec04cbb395
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2022-08-07 11:18:17 +0700

    Initial commit

---
+++ SK-Lib-stdio.h-puts-Bagian7__C/Source.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*
+    Source by Microsoft (https://docs.microsoft.com/en-us/cpp)
+    Modified For Learn by RK
+    I.D.E : VS2022
+*/
+
+int main() {
+	puts("Halo Manusia, !");
+
+    _getch();
+    return 0;
+}
